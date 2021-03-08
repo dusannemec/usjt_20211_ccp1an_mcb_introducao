748b0f507dee12f735df4ea4235ace0b8b23f305
Rodrigo Lins <rodrigolins.rsa@gmail.com>
2021-03-08 20:21:34 -0300

    Cálculo de salário de acordo com o tipo

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,24 @@
 public class Empregado {
-    public String nome;
-    public int idade;
+
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
+    private String nome;
+    private int idade;
+
+    public double calcularSalario() {
+        switch (tipo) {
+            case 1:
+                return salario;
+            case 2:
+                return salario + salario * comissao;
+            case 3:
+                return salario + bonus;
+            default:
+                return 0;
+        }
+    }
 
     public int getIdade() {
         return idade;
@@ -11,7 +29,7 @@ public class Empregado {
     }
 
     public void setIdade(int idade) {
-        this.idade =  idade;
+        this.idade = idade;
     }
 
     public void setNome(String nome) {
